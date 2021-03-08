c5859223e2fd6f4ce904f766d3db0be9c6b3dd0e
StasBeep <stas-stas.stanislav@mail.ru>
2021-03-08 18:52:20 +0300

    Считывание и запись изменений с помощью сторонних файлов x2

---
+++ Laba12Var24/Laba12Var24/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Laba12Var24
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string data = openFileDialog1.FileName;
+                StreamReader yk = new StreamReader(data);
+                string Onedata = yk.ReadToEnd();
+                yk.Close();
+                textBox2.Text = Onedata;
+            }
+        }
+        StrInPOut pro = new StrInPOut();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string DataRead = openFileDialog1.FileName;
+                StreamReader yk = new StreamReader(DataRead);
+                while (!yk.EndOfStream)
+                {
+                    pro.s = yk.ReadLine();
+                    pro.CallPublMetod(pro.s);
+                    pro.OutPut(listBox1);
+                    listBox2.Items.Add(pro.s);
+                }
+                yk.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveText = new SaveFileDialog();
+            if(SaveText.ShowDialog() == DialogResult.OK)
+            {
+                string str = SaveText.FileName;
+                StreamWriter Wrt = new StreamWriter(str);
+                for (int i = 0; i <= listBox1.Items.Count - 1; i++)
+                    Wrt.WriteLine(listBox1.Items[i]);
+                Wrt.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Laba12Var24/Laba12Var24/SsOperations.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba12Var24
+{
+    public class SsOperations
+    {
+        public string s;
+        protected int dd;
+        protected int mm;
+        protected int yy;
+        public int ForvardINT;
+        private int[] MassMM;
+        public int day;
+        public int month;
+        public int year;
+
+        private void bissextile()
+        {
+            MassMM = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (yy % 4 == 0 && yy % 100 != 0)
+                MassMM[1] = 29;
+            if (yy % 100 == 0 && yy % 400 == 0)
+                MassMM[1] = 29;
+        }
+        protected void MonthOperations()
+        {
+            bissextile();
+            int differencedd = MassMM[mm - 1] - dd;
+            if (ForvardINT > differencedd)
+            {
+                ForvardINT -= differencedd;
+                mm++;
+                dd = 1;
+            }
+            else
+            {
+                day = dd + ForvardINT;
+                ForvardINT = 0;
+                month = mm;
+                year = yy;
+            }
+            while (ForvardINT >= 0)
+            {
+                if (mm > 12)
+                {
+                    mm = 1;
+                    yy++;
+                    bissextile();
+                }
+                if (MassMM[mm - 1] <= ForvardINT)
+                {
+                    ForvardINT -= MassMM[mm - 1];
+                    mm++;
+                }
+                else
+                {
+                    day = dd + ForvardINT;
+                    month = mm;
+                    year = yy;
+                    break;
+                }
+            }
+        }
+    }
+    public class StrInPOut:SsOperations
+    {
+        public void CallPublMetod(string str)
+        {
+            dd = Convert.ToInt32(str.Substring(0, 2));
+            mm = Convert.ToInt32(str.Substring(3, 2));
+            yy = Convert.ToInt32(str.Substring(6, 4));
+            ForvardINT = Convert.ToInt32(str.Substring(11));
+            MonthOperations();
+        }
+        public void OutPut(ListBox pro)
+        {
+            string dayStr = day.ToString();
+            string monthStr = month.ToString();
+            if (day > 0 && day < 10)
+                dayStr = "0" + day;
+            if (month > 0 && month < 10)
+                monthStr = "0" + month;
+            pro.Items.Add(dayStr + "." + monthStr + "." + yy.ToString());
+        }
+    }
+}
