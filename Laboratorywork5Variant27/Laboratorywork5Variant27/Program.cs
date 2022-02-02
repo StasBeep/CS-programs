1f141fc48b9113fda94a1e2b5f86bee31a573526
StasBeep <stas-stas.stanislav@mail.ru>
2022-02-02 23:22:19 +0300

    Лабораторная работа 5 варината 27 (готова)

---
+++ Laboratorywork5Variant27/Laboratorywork5Variant27/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorywork5Variant27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // подключение класса branching (branching() - конструктор)
+            // Obj - переменная обращения к классу branching
+            // с помощью неё происходит запрос к public данным
+            branching Obj = new branching();
+
+            // запрос входных данных
+            Console.WriteLine("Введите с: ");
+            Obj.c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y: ");
+            Obj.y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите x: ");
+            Obj.x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите z: ");
+            Obj.z = Convert.ToDouble(Console.ReadLine());
+
+            // вычисление результата через метод systemSolution
+            Obj.l = Obj.systemSolution(Obj.c, Obj.y, Obj.x, Obj.z);
+
+            // очистка консоли
+            Console.Clear();
+
+            // вывод результата на консоль
+            Console.WriteLine("При с = " + Obj.c + " при y = " + Obj.y + " при x = " + Obj.x + " при z =" + Obj.z);
+            Console.WriteLine("Система принимает результат: " + Obj.l);
+
+            // задержка
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laboratorywork5Variant27/Laboratorywork5Variant27/branching.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorywork5Variant27
+{
+    class branching
+    {
+        // входные данные (public - для того, чтобы видели другие пространства)
+        public double c, y, x, z, l;
+
+        // открытый метод (public)
+        public double systemSolution(double c, double y, double x, double z)
+        {
+            // возвращаемое значение (третье условие выполняется автоматически)
+            double l = 15;
+
+            // Условия
+            if (y * z > 0 && x > 0)
+            {
+                double max = Math.Sqrt(x);
+
+                if (max < c * y)
+                {
+                    max = c * y;
+                }
+
+                double min = y;
+
+                if (min > max)
+                {
+                    min = max;
+                }
+                if (min > z)
+                {
+                    min = z;
+                }
+
+                l = min;
+            }
+            else if (y * z < 0 && x > 0)
+            {
+                double min = Math.Tan(x);
+
+                if (min > Math.Pow(z, 3))
+                {
+                    min = Math.Pow(z, 3);
+                }
+
+                l = min;
+            }
+
+            return l;
+        }
+    }
+}
