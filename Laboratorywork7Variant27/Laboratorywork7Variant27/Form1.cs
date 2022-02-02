fec19396193fad0a5d0a61d15e89e78346bdd44a
StasBeep <stas-stas.stanislav@mail.ru>
2022-02-03 01:43:40 +0300

    Лабораторная работа №7 варината 27 (готова)

---
+++ Laboratorywork7Variant27/Laboratorywork7Variant27/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorywork7Variant27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Подкючение к классу Raz
+            Raz Obj = new Raz();
+
+            Obj.a = Convert.ToDouble(textBox1.Text);
+            Obj.b = Convert.ToDouble(textBox2.Text);
+            Obj.h = Convert.ToDouble(textBox3.Text);
+
+            // Очистка listBox1
+            listBox1.Items.Clear();
+
+            // Использование метода Fx() класса Raz
+            Obj.Fx(Obj.a, Obj.b, Obj.h, listBox1);
+        }
+    }
+}
---
+++ Laboratorywork7Variant27/Laboratorywork7Variant27/Raz.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Подключение Form к классу Raz
+using System.Windows.Forms;
+
+namespace Laboratorywork7Variant27
+{
+    class Raz
+    {
+        // Входные данные
+        public double a, b, h, y;
+
+        /* Метод вычисления значения функции одного аргумента в
+         * одной точке (используется private - закрытый доступ,
+         * работа только внутри класса)
+         * @param {double} X - перменная (одна точка)
+         * @return {double} - возврат значения функции 
+         * P.S. 1.0/3.0 - обязательно прописывать через ".", так
+         * как тип степени double, если использовать int, то
+         * резултат будет считаться не правильно
+         */
+        private double OnePoint(double X)
+        {
+            if(X - 1 < 0)
+            {
+                return X * (-1) * Math.Pow(Math.Abs(X - 1), (1.0 / 3.0));
+            }else if(X == 1)
+            {
+                return 0;
+            } else
+            {
+                return X * Math.Pow((X - 1), (1.0 / 3.0));
+            }
+        }
+
+        // Метод запуска и вывода результата функции в ListBox
+        public void Fx (double a, double b, double h, ListBox LBx1)
+        {
+            // цикл do...while()
+            do
+            {
+                // использование метода OnePoint
+                y = OnePoint(a);
+                // вывод значений в ListBox
+                LBx1.Items.Add("f( " + a + " )  = " + y);
+                // изменение шага цикла
+                a += h;
+            } while (a <= b);
+        }
+    }
+}
