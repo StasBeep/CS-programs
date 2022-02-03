afe61679f2d4e7126dae479d510010a95c3d0c66
StasBeep <stas-stas.stanislav@mail.ru>
2022-02-03 19:15:20 +0300

    Лабораторная работа №9 варианта 27 (готова)

---
+++ Laboratorywork9Variant27/Laboratorywork9Variant27/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Laboratorywork9Variant27
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 303);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "-1\r\n0\r\n10\r\n-3\r\n-5\r\n6\r\n-2\r\n3\r\n4";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(319, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 303);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Решение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Исходный массив";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Массив индексов ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(152, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Сумма:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(202, 232);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 301);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 342);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Laboratorywork9Variant27/Laboratorywork9Variant27/Operations.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Подключение для работы с Forms
+using System.Windows.Forms;
+
+namespace Laboratorywork9Variant27
+{
+    // Класс родитель
+    public class Operations
+    {
+        // входные и выходные данные
+        public double[] a;
+        public double[] b;
+        public double S;
+        public int n;
+
+        // метод вычисления суммы элементов с
+        // нечётными номерами
+        protected double SumElement()
+        {
+            // начальное значение суммы равно 0
+            this.S = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 1)
+                    S += a[i];
+            }
+            return this.S;
+        }
+
+        // метод вычисляющий количество положительных
+        // элементов массива
+        private int NumberEl()
+        {
+            this.n = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] >= 0)
+                    n++;
+            }
+            return this.n;
+        }
+
+        // формирование массива из индексов положительных
+        // элементов исходного массива
+        protected double[] ArrayIndex()
+        {
+            this.n = NumberEl();
+            b = new double[this.n];
+            int k = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                if(a[i] >= 0)
+                {
+                    b[k] = i + 1;
+                    k++;
+                }
+
+            }
+            return b;
+        }
+    }
+
+    // Класс наследник (наследник от класса Operations)
+    public class enter:Operations
+    {
+        // метод вызова методов выполняющих поставленную задачу
+        public void demand()
+        {
+            this.S = SumElement();
+            this.b = ArrayIndex();
+        }
+
+        // метод заполнения массива исходными данными
+        // возвращается массив a типа double
+        public double[] Input(TextBox TxBx)
+        {
+            // определение количества строк в textbox (TxBx)
+            this.n = TxBx.Lines.Count();
+            // объявление нового одномерного массива a []
+            a = new double[n];
+            // заполнение массива а данными из textbox
+            for (int i = 0; i < a.Length; i++)
+                a[i] = double.Parse(TxBx.Lines[i]);
+            return b;
+        }
+
+        // метод вывода массива с результатом
+        public void Output(ListBox LB, double[] array)
+        {
+            // Очистка полей listbox
+            LB.Items.Clear();
+            for (int i = 0; i < array.Length; i++)
+                LB.Items.Add(array[i]);
+        }
+    }
+}
