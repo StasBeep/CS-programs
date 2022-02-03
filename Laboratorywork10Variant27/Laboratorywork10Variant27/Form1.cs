ecf49e084b6bea7830345b1733dd02fb859835fd
StasBeep <stas-stas.stanislav@mail.ru>
2022-02-03 22:26:25 +0300

    Лабораторная работа №10 варианта 27 (готова)

---
+++ Laboratorywork10Variant27/Laboratorywork10Variant27/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorywork10Variant27
+{
+    public partial class Form1 : Form
+    {
+        // подключение класса operations для всех точек
+        ResInpOut Obj = new ResInpOut();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Obj.N = Convert.ToInt32(textBox1.Text);
+            Obj.A = Obj.InpMatrix(listBox1);
+            Obj.B = Obj.InpMatrix(listBox2);
+            Obj.C = Obj.InpMatrix(listBox3);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Obj.purpose(Obj.A, Obj.B ,Obj.C, listBox4);
+        }
+    }
+}
---
+++ Laboratorywork10Variant27/Laboratorywork10Variant27/Operations.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// подключение элементов формы
+using System.Windows.Forms;
+
+namespace Laboratorywork10Variant27
+{
+    public class Operations
+    {
+        // количество строк и столбцов n*n
+        public int N
+        {
+            get { return n; }
+            set { n = value; }
+        }
+
+        public double[,] A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public double[,] B
+        {
+            get { return b; }
+            set { b = value; }
+        }
+
+        public double[,] C
+        {
+            get { return c; }
+            set { c = value; }
+        }
+
+        public double[,] X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        // количество строк и столбцов (private)
+        private int n;
+
+        // матрицы
+        private double[,] a;
+        private double[,] b;
+        private double[,] c;
+        private double[,] x;
+
+        // метод вычисления наибольшего значения элемента из матриц
+        protected double[,] MaxElement(double[,] a,double[,] b,double[,] c)
+        {
+            x = new double[n, n];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    double max = a[i, j];
+                    if (max < b[i, j])
+                        max = b[i, j];
+                    if (max < c[i, j])
+                        max = c[i, j];
+                    x[i, j] = max;
+                }
+            }
+
+            return this.x;
+        }
+    }
+
+    public class ResInpOut : Operations
+    {
+        // конструктор для рандома 
+        Random rand = new Random();
+        
+        // метод заполнения матриц
+        public double[,] InpMatrix(ListBox lb)
+        {
+            double[,] array = new double[N, N];
+            for(int i = 0; i < N; i++)
+            {
+                for(int j = 0; j < N; j++)
+                {
+                    array[i, j] = Math.Round(rand.NextDouble() * 100, 2);
+                }
+            }
+
+            OutMatrix(array, lb);
+
+            return array;
+        }
+
+        // Метод вывода двумерной матрицы
+        private void OutMatrix(double[,] array, ListBox lb)
+        {
+            lb.Items.Clear();
+            for(int i = 0; i < N; i++)
+            {
+                string s = "";
+                for(int j = 0; j < N; j++)
+                {
+                    s += array[i, j].ToString() + "  ";
+                }
+                lb.Items.Add(s);
+            }
+
+        }
+
+        public void purpose(double[,] a, double[,] b, double[,] c, ListBox lb)
+        {
+            this.X = MaxElement(a, b, c);
+            OutMatrix(this.X, lb);
+        }
+    }
+}
